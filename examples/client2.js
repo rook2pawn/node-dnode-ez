901f3f233b11e916f89618399e4fd6fbc5312444
david wee <rook2pawn@gmail.com>
2012-03-22 07:39:55 -0700

    updated for new cool examples

--- examples/client2.js
+++ examples/client2.js
@@ -1,8 +1,32 @@
 var dnode_ez = require('../index');
 var EE = require('events').EventEmitter;
 var emitter = new EE;
-emitter.on('wow',function() {console.log("Wow. Just wow.");});
+emitter.on('wow',function(arg1) {console.log("Wow. Just wow."); console.log("Arg1: " + arg1)});
+var emitter2 = new EE;
+emitter2.on('cool',function(arg1) {console.log("Super cool."); console.log("Arg1: " + arg1)});
 var client = dnode_ez();
 client.connect(5050);
-client.emit('foobar',' <-- nice!');
 client.bind(emitter,'justAnotherEmitter');
+client.emit('foobar',' <-- nice!');
+client.bind(emitter2,'woohoo');
+console.log("Press c to emit foobar on server emitter 1");
+var tty = require('tty');
+process.stdin.resume();
+tty.setRawMode(true);
+process.stdin.on('keypress', function(char, key) {
+    if (key && key.ctrl && key.name == 'c') {
+        console.log('graceful exit');
+        process.exit()
+    }   
+    if (key !== undefined) {
+        console.log("You entered -> " + key.name);
+        switch (key.name) {
+        case 'c' :
+                client.emit('foobar','Really man?');
+                break;
+        default :
+                break;
+        }
+    }
+});
+
--- examples/server2.js
+++ examples/server2.js
@@ -2,7 +2,45 @@ var dnode_ez = require('../index');
 var server = dnode_ez();
 server.on('foobar',function(val) {console.log("Server foobar! "+val);});
 server.listen(5050);
+var foo = undefined;
+var bar = undefined;
 server.on('bind',function(id) {
 	var emitterOnClient = server.getEmitter(id);
-	emitterOnClient.emit('wow');
+    switch (emitterOnClient.id) {
+        case 'justAnotherEmitter' : emitterOnClient.emit('wow');
+                foo = emitterOnClient;
+                break;
+        case 'woohoo' : emitterOnClient.emit('cool');
+                bar = emitterOnClient;
+                break;
+        default :
+                break;
+    }
+});
+
+
+console.log("Press a to emit wow on client emitter 1");
+console.log("Press b to emit cool on client emitter 2");
+console.log("Press c to emit foobar on server emitter 1");
+var tty = require('tty');
+process.stdin.resume();
+tty.setRawMode(true);
+process.stdin.on('keypress', function(char, key) {
+    if (key && key.ctrl && key.name == 'c') {
+        console.log('graceful exit');
+        process.exit()
+    }
+    if (key !== undefined) {
+        console.log("You entered -> " + key.name);
+        switch (key.name) {
+        case 'a' : 
+                foo.emit('wow','The First Argument');
+                break;
+        case 'b' :
+                bar.emit('cool','iseewhatyoudidthere');
+                break;
+        default :
+                break;
+        }
+    } 
 });
