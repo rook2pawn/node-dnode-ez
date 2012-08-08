e1d76778166d7c1b9789fb30ec8fe0479deb4735
David Wee <rook2pawn@gmail.com>
2012-08-08 08:15:08 -0700

    added server primitive test

---
+++ test/testServerPrimitiveEvent.js
@@ -0,0 +1,23 @@
+// test that a primitive event can be emitted from the client 
+// side. a primitive event is one where there is no actual event emitter
+// but merely saying server.on('foo', fn) and client.emit('foo')
+var test = require('tap').test;
+var ez = require('../index');
+
+test('testServerPrimitiveEvent', function(t) {
+    t.plan(1);
+    var d = ez();
+    d.listen(12345);
+    d.on('newClient',function(msg) {
+        console.log("new client says:\n");
+        console.log(msg);
+        t.equal(msg, 'My name is Foo Bar');
+        t.end();
+        d.close();
+    });
+    var c = ez();
+    c.connect(12345);
+    c.on('connect',function(remote,conn) {
+        remote.emit('newClient','My name is Foo Bar');
+    });
+});
